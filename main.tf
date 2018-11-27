37add86bab93f62cff067eea407eb320405f2763
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-11-27 03:27:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
+variable count {
+  description = "How many pets"
+  default     = 60
+}
+
 resource "random_pet" "pet_quote_1" {
-  count = 1000
+  count = "${var.count}"
 }
 
 resource "random_pet" "pet_quote_2" {
-  count = 1000
+  count = "${var.count}"
 }
 
 resource "random_pet" "pet_quote_3" {
-  count = 1000
+  count = "${var.count}"
 }
